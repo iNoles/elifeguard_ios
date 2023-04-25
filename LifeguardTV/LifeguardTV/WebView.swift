10b59d7c2660f0e0324cbf1ced0932fc5943a91d
Jonathan Steele <it@elifeguard.com>
2023-04-25 16:37:15 -0400

    Oops, forget to add these files

---
+++ LifeguardTV/LifeguardTV/WebView.swift
@@ -0,0 +1,30 @@
+//
+//  WebView.swift
+//  LifeguardTV
+//
+//  Created by TECH1 on 4/25/23.
+//
+
+import UIKit
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    private let webview = WKWebView()
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return webview
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(URLRequest(url: URL(string: "https://www.lifeguardtv.com/")!))
+    }
+    
+    func goBack() {
+        webview.goBack()
+    }
+
+    func goForward() {
+        webview.goForward()
+    }
+}
